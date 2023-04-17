a1c922d12a8da0569edc64758ada9ef7e45570da
Ricky <ruei7916@gmail.com>
2023-04-17 12:10:50 +0800

    init

---
+++ build/CMakeFiles/adder-qk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/adder-qk.dir/main.cpp.o"
+  "CMakeFiles/adder-qk.dir/main.cpp.o.d"
+  "adder-qk"
+  "adder-qk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/adder-qk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
